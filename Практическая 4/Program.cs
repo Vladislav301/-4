05e39abab72b3b406cf97b73d8ef0043237eb512
Vladislav301 <gordeevvlad151@gmail.com>
2024-11-25 13:33:19 +0900

    Add files via upload

---
+++ Практическая 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class DivisionProgram
+{
+    public static double GetNumberFromUser(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            if (double.TryParse(Console.ReadLine(), out double number))
+            {
+                return number;
+            }
+            Console.WriteLine("Неверный формат числа. Повторите ввод.");
+        }
+    }
+
+
+    public static void Main(string[] args)
+    {
+        try
+        {
+            double num1 = GetNumberFromUser("Введите первое число которрое вам нравится: ");
+            double num2 = GetNumberFromUser("Введите второе число которое вам не нравится: ");
+            double result = num1 * num2;
+            Console.WriteLine($"Результат который вам может понравится или нет: {result}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла неизвестная ошибка: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Программа завершена. Спасибо за работу!");
+        }
+    }
+}
+
+
